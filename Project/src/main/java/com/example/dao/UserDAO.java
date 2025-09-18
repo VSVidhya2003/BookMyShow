2942586b0600a5b0d8e62849c1c20d3c2acb1c44
VSVidhya2003 <vsaividhya2003@gmail.com>
2025-09-18 16:04:44 +0530

    Delete Project directory

--- Project/src/main/java/com/example/dao/UserDAO.java
+++
@@ -1,8 +0,0 @@
-package com.example.dao;
-
-import com.example.dto.UserDTO;
-
-public interface UserDAO {
-    boolean registerUser(UserDTO user);
-    UserDTO loginUser(String email, String password);
-}
